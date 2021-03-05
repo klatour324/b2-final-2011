9ea1a12d17c6ea363fed9692b5ccdff6ae620454
Katy La Tour <klatour324@gmail.com>
2021-03-05 10:58:36 -0600

    Add AR method to Flight model for ordering by departure city from A-Z

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def self.order_by_departure
+    order(departure_city: :asc)
+  end
 end
