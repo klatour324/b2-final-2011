65d921984689a8a4f001adc94e564053da1f823d
Katy La Tour <klatour324@gmail.com>
2021-03-05 12:24:13 -0600

    Add show action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,10 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.order_by_departure
   end
+
+  def show
+    @flight = Flight.find(params[:id])
+    @flight.passengers.eighteen_and_older
+    @flight.average_age_of_adult_passengers
+  end
 end
