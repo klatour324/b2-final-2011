7962daddf66f057093f2ee9da560a4d3c36cca65
Katy La Tour <klatour324@gmail.com>
2021-03-05 10:24:56 -0600

    Create the models specs for many to many relationship between flights and passengers

---
+++ spec/models/flight_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Flight, type: :model do
+  describe "relationships" do
+    it { should have_many :flight_passengers}
+    it { should have_many(:passengers).through(:flight_passengers)}
+  end
+end
